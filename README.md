4920cf3518105816525eae74426ecaa70a75087f
Jacopo Di Ventura <jacopo.diventura@gmail.com>
2020-11-17 12:06:30 +0100

    file selection

---
+++ README.md
@@ -0,0 +1,20 @@
+# ejs precompile
+
+#### Usage: 
+
+* how to add files or directories:
+
+  ##### Add a file: 
+
+  add an element to the files array (in ```files.json```) with the path of the file you want to compile relative to ```ejs-compile.js``` , without ```./``` . Examples: if the file is in the same folder you would simply write ```filename.js``` if the file is in a subfolder you would write ```sub1/sub2/filename.js```
+
+  
+
+  ##### Add a directory:
+
+  add an element to the dirs array (in ```files.json```) with the path of the directory containing all the files you want to compile relative to ```ejs-compile.js``` , without ```./```  at the start of the string but with a ```/``` at the end. 
+
+  
+
+* now you can run the file however you like and the compiled views will be written to ```public/js/views.js```
+
---
+++ ejs-compile.js
@@ -0,0 +1,57 @@
+/**
+ * EJS Compiler
+ *
+ * Compile all views/*.ejs files into a single /public/js/views.js file
+ */
+
+const fs = require("fs-extra");
+const ejs = require("ejs");
+const glob = require("glob-fs")({ gitignore: true });
+const json_file = require("./files.json");
+
+// console.log(json_file.dirs);
+
+let x = [];
+
+json_file.dirs.forEach((dir) => {
+  x = x.concat(
+    fs.readdirSync(dir).map((e) => {
+      return dir + e;
+    })
+  );
+});
+x = x.concat(json_file.files);
+
+function compile(view) {
+  let v = view.replace(/\//g, "_").replace(/\.ejs$/, "");
+
+  console.log(v);
+
+  let template = new String(fs.readFileSync(view));
+  let f = ejs.compile(template, { client: true });
+
+  let f_str = f.toString();
+
+  f_str = f_str
+    .replace("function anonymous(", "ejs." + v + " = function(")
+    .replace(
+      "function(locals, escapeFn, include, rethrow",
+      "function(locals, escapeFn, include = ejs.views_include(locals), rethrow"
+    );
+
+  return f_str;
+}
+
+let compiled = x.map(compile).join("\n\n");
+
+let output = `//EJS Compiled Views - This file was automatically generated on ${new Date()}
+ejs.views_include = function(locals) {
+    return function(path, d) {
+        return ejs["views_"+path.replace(/\\\//g,"_")]({...d,...locals}, null, ejs.views_include(locals));
+    }
+};
+${compiled}`;
+
+console.log(output);
+
+// fs.writeFile("public/js/views.js", output);
